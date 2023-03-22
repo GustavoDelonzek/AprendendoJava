a52c147a2e51f7474a1a750d18a466a8aa975cb3
Gustavo Delonzek <gustavo2016delonzek@gmail.com>
2023-03-22 19:56:32 -0300

    Aula herança parte 2

---
+++ src/aula11/Aula011.java
@@ -0,0 +1,17 @@
+package aula11;
+
+public class Aula011 {
+
+	public static void main(String[] args) {
+		Aluno a1 = new Aluno();
+		a1.setNome("Gustavo");
+		a1.setMatricula(1111);
+		a1.pagarMensalidade();
+		Bolsista a2 = new Bolsista();
+		a2.setNome("Alfredo");
+		a2.setBolsa(12);
+		a2.setSexo("m");
+		a2.pagarMensalidade();
+	}
+
+}
